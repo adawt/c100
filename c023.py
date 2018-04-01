94147c2928f8d7e9148612bdd33fcb60f617d36b
adawt <872726385@qq.com>
2018-04-02 00:57:58 +0800

    c015

--- c023.py
+++ c023.py
@@ -13,3 +13,20 @@
 　　　　　　for循环，第一层控制行，第二层控制列。 
 2.程序源代码： 
 """
+# for i in range(1, 9, 2):
+#     print(' '*((7-i)//2), end='')
+#     print('*'*i)
+# for j in range(5, -1, -2):
+#     print(' ' * ((7 - j) // 2), end='')
+#     print('*'*j)
+n = int(input('n =:\n'))
+if n % 2 != 0:
+    for i in range(1, n+2, 2):
+        print(' '*((n-i)//2), end='')
+        print('*'*i)
+    for j in range(n-2, -1, -2):
+        print(' ' * ((n - j) // 2), end='')
+        print('*'*j)
+else:
+    print("error")
+
